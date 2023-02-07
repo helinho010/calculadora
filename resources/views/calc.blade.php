5197f2d648c4e3be0c50c75213e36db1092c1f79
Helio <mm.helio009@gmail.com>
2023-02-07 13:36:57 -0400

    funciona solo presionando los botones, pero al poner con el teclado numerico no!

--- resources/views/calc.blade.php
+++ resources/views/calc.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset("css/estilos.css") }}">
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>CALCULADORA</title>
 </head>
 <body>
     <div class="container text-center">
@@ -70,11 +70,15 @@
     $("button").click(function(e){
         e.preventDefault();
         var expresion=String($('#texto-expresion').val());
-        if ($(this).attr('name') == "calcular"){
+        var atributoNameButton= $(this).attr('name');
+        console.log(expresion);
+        console.log(atributoNameButton);
+        console.log($('#texto-expresion').attr('value','0'));
+        if ( atributoNameButton == "calcular"){
             $('#texto-expresion').attr('value',eval(expresion));
-        }else if ($(this).attr('name') == "C") {
+        }else if (atributoNameButton == "C") {
             $("#texto-expresion").attr("value","0");
-        }else if ($(this).attr('name') == "R"){
+        }else if (atributoNameButton == "R"){
             expresion=expresion.substring(0,expresion.length-1);
             if (expresion === ""){
               expresion="0";  
@@ -88,11 +92,8 @@
             expresion = expresion + $(this).attr('name');
             $('#texto-expresion').attr('value',expresion);
         }
-        $('#texto-expresion').attr('value',expresion);
     });
 </script>
-        
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
